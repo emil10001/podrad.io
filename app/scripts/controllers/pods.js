633be64d3f2a66f8967fb54668320daa38a6af82
John F <ejohn@feigdev.com>
2013-06-09 14:46:33 -0700

    delete method and icons

--- app/scripts/controllers/pods.js
+++ app/scripts/controllers/pods.js
@@ -1,16 +1,20 @@
 'use strict';
 
 angular.module('intellijWorkspaceApp')
-    .controller('PodsCtrl', function ($scope) {
-        $scope.myPods = [
-            {'name' : 'Planet Money',
+.controller('PodsCtrl', function ($scope) {
+    $scope.myPods = [
+        {'name' : 'Planet Money',
             'url' : 'http://www.npr.org/rss/podcast.php?id=510289'},
             {'name' : 'Dunk Tank w/ Brian and Chet',
-            'url' : 'http://www.dunktankpodcast.com/1/feed'}
-        ];
-        $scope.addPod = function(){
-            $scope.myPods.push({'name': $scope.name, 'url': $scope.url});
-            $scope.name = '';
-            $scope.url = '';
-        };
-    });
+                'url' : 'http://www.dunktankpodcast.com/1/feed'}
+    ];
+    $scope.deletePod = function(toDelete){
+        if (confirm('really delete ' + toDelete.name + '?'))
+            $scope.myPods.splice($scope.myPods.indexOf(toDelete),1);
+    };
+    $scope.addPod = function(){
+        $scope.myPods.push({'name': $scope.name, 'url': $scope.url});
+        $scope.name = '';
+        $scope.url = '';
+    };
+});
